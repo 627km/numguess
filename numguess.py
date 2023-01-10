f4a9bc6e6590c68636c5d1ffa363f94aa759f300
627km <rudals_jeon@naver.com>
2023-01-10 15:08:39 +0900

    feat: Add comparing user_answer with answer
    만약에 유저의 답변이 정답과 같다면 축하메세지를 출력.
    그렇지 않은 경우 중,
    유저의 답변이 정답보다 크다면, 크다는 메세지를
    유저의 답변이 정답보다 작다면, 작다는 메세지를 출력한다.

--- numguess.py
+++ numguess.py
@@ -1,4 +1,5 @@
 from random import randint
+from time import sleep
 
 username = input("Enter your name > ")
 print(username, "Welcome! We start game! Guess number!")
@@ -10,3 +11,17 @@ print(answer)
 user_answer = int(input(f"{username}, Guess number (1~100) > "))
 
 print(f"Okay, your choice is {user_answer}")
+
+# Compare answer with user_answer
+if user_answer == answer:
+    print('***************************')
+    sleep(1)
+    print('***************************')
+    sleep(1)
+    print('***************************')
+    sleep(1)
+    print(f'You got it right!! The answer is {answer}!!')
+elif user_answer > answer:
+    print(f'Keep going ~, that was too high!')
+elif user_answer < answer:
+    print(f'Kepp going ~, that was too low!')
