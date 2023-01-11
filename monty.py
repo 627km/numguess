1a240fb5ded1482ccb094a2fd596d5f1c85d81fb
627km <rudals_jeon@naver.com>
2023-01-11 10:03:28 +0900

    feat: Divide 2 case choice 1 and choice 0
    Generate if statement

--- monty.py
+++ monty.py
@@ -8,4 +8,7 @@ door = [0,0,1]
 for i in range(1, total_repeat+1):
     shuffle(door)
     user_choice = choice(door)
-
+    if user_choice == 1:
+        nochange_for_success += 1
+    elif user_choice == 0:
+        change_for_success += 1
