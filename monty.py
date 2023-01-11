8578147a8222efb1933f1929f9ca3733c5992a65
627km <rudals_jeon@naver.com>
2023-01-11 10:08:32 +0900

    feat: Show result
    calculate percentage
    show result percentage

--- monty.py
+++ monty.py
@@ -12,3 +12,7 @@ for i in range(1, total_repeat+1):
         nochange_for_success += 1
     elif user_choice == 0:
         change_for_success += 1
+change_percent = change_for_success / total_repeat
+nochange_percent = nochange_for_success / total_repeat
+print(f'바꾸지 않을 경우 이길 확률 = {nochange_percent}')
+print(f'바꿀 경우 이길 확률 = {change_percent}')
