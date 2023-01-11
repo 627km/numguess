a9121363684d9a17346f5307bb148dcbcc1eb6d4
627km <rudals_jeon@naver.com>
2023-01-11 10:10:58 +0900

    feat: Change Korean to English

--- monty.py
+++ monty.py
@@ -14,5 +14,5 @@ for i in range(1, total_repeat+1):
         change_for_success += 1
 change_percent = change_for_success / total_repeat
 nochange_percent = nochange_for_success / total_repeat
-print(f'바꾸지 않을 경우 이길 확률 = {nochange_percent}')
-print(f'바꿀 경우 이길 확률 = {change_percent}')
+print(f'nochange for success percentage = {nochange_percent}')
+print(f'change for success percentage = {change_percent}')
